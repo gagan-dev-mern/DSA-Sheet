11d4a268affb50550cc64664910b842155f324bb
Gagan Aggarwal <113084015+gaganeducase@users.noreply.github.com>
2025-09-07 22:13:26 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,12 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use(cors({
+    origin: [
+        'http://localhost:3000'
+    ],
+    credentials: true
+}));
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
